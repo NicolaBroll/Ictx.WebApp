0e28fe1ccfa8c98c9bfdc44d07f34cc6a749954a
Nicola <nicola.brl94@gmail.com>
2021-02-18 08:45:03 +0100

    Start integration test

---
+++ test/Ictx.WebApp.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ictx.WebApp.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
