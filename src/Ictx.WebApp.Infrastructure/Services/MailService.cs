45f1af431d2cf994a3958aea277033994813ed52
Nicola <nicola.brl94@gmail.com>
2021-05-18 18:24:09 +0200

    Try

--- src/Ictx.WebApp.Infrastructure/Services/MailService.cs
+++ src/Ictx.WebApp.Infrastructure/Services/MailService.cs
@@ -6,34 +6,45 @@ using Ictx.WebApp.Application.Common;
 using Ictx.WebApp.Core.Models;
 using Ictx.WebApp.Core.Interfaces;
 using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace Ictx.WebApp.Infrastructure.Services
 {
     public class MailService: IMailService
     {
-        private readonly IMailSettings _mailSettings;
+        private readonly ILogger<MailService>   _logger;
+        private readonly IMailSettings          _mailSettings;
 
-        public MailService(IMailSettings mailSettings)
+        public MailService(ILogger<MailService> logger, IMailSettings mailSettings)
         {
-            this._mailSettings = mailSettings;
+            this._logger        = logger;
+            this._mailSettings  = mailSettings;
         }
 
         public async Task SendEmail(List<MailModel> mails)
         {
-            using (var client = new SmtpClient())
+            try
             {
-                // For demo-purposes, accept all SSL certificates
-                //client.ServerCertificateValidationCallback = (_, _, _, _) => true;
-
-                await client.ConnectAsync(this._mailSettings.IpAddress, this._mailSettings.Port, this._mailSettings.UseSsl);
-
-                foreach (var mail in mails)
+                using (var client = new SmtpClient())
                 {
-                    await SendEmail(mail, client);
-                }
+                    // For demo-purposes, accept all SSL certificates
+                    //client.ServerCertificateValidationCallback = (_, _, _, _) => true;
 
-                await client.DisconnectAsync(true);
-            };
+                    await client.ConnectAsync(this._mailSettings.IpAddress, this._mailSettings.Port, this._mailSettings.UseSsl);
+
+                    foreach (var mail in mails)
+                    {
+                        await SendEmail(mail, client);
+                    }
+
+                    await client.DisconnectAsync(true);
+                };
+            }
+            catch (Exception e)
+            {
+                this._logger.LogError(e, "MailService.SendEmail");
+            }           
         }
 
         public async Task SendEmail(MailModel mail)
